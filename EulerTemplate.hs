688a6c8930148f1b27a1b2403cd99d6ff76ccfc0
Crockeo <crockeo@gmail.com>
2012-11-18 02:18:58 -0500

    Setting custom module name.

--- EulerTemplate.hs
+++ EulerTemplate.hs
@@ -1,3 +1,4 @@
+
 -------------------------------
 -- Project Euler Challenge File
 -------------------------------
@@ -5,6 +6,10 @@
 -- Date started: 
 -- Date ended:
 -------------------------------
+
+-- Change the N to the current problem
+module EulerN where
+
 
 
 -- Call the main function to get the result
