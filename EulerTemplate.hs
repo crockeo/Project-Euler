94cee5c7c1d857de7579f7a78e6357a536a3307e
Crockeo <crockeo@gmail.com>
2012-11-18 02:15:58 -0500

    Adding the template.

---
+++ EulerTemplate.hs
@@ -0,0 +1,11 @@
+-------------------------------
+-- Project Euler Challenge File
+-------------------------------
+-- Number: 
+-- Date started: 
+-- Date ended:
+-------------------------------
+
+
+-- Call the main function to get the result
+main = undefined
