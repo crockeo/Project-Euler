4b65a87955664417ec6666947371846c57801a82
Crockeo <crockeo@gmail.com>
2013-07-01 01:16:42 -0400

    Revised the solution to 1. not run into memory limits in ghci and 2. work at the speed of ~3 factorial functions

--- Euler015.hs
+++ Euler015.hs
@@ -3,41 +3,18 @@
 -------------------------------
 -- Number: 15
 -- Time started: 2:06 AM - 6/30/2013
--- Time ended:
+-- Time ended: 12:58 AM - 7/1/2013
 -------------------------------
 
 -- Change the N to the current problem
 module Euler15 where
 
-import Control.Monad
-import Data.List
-
+fac :: Integer -> Integer
 fac 0 = 1
-fac n = n * fac (n - 1)
+fac n = n * (fac (n - 1))
 
-data Direction = GoRight | GoDown
-  deriving (Eq, Show, Read)
-type Point = (Integer, Integer)
-
-translate :: Point -> Point -> Point
-translate (x1, y1) (x2, y2) = (x1 + x2, y1 + y2)
-
-toPoint :: Direction -> Point
-toPoint GoRight = (1, 0)
-toPoint GoDown = (0, 1)
-
-moveDirection :: Direction -> Point -> Point
-moveDirection dir point = translate point $ toPoint dir
-
-reachesEndRaw :: Point -> [Direction] -> Point -> Bool
-reachesEndRaw point [] end = point == end
-reachesEndRaw point (x:xs) end = reachesEndRaw (moveDirection x point) xs end
-
-reachesEnd :: [Direction] -> Point -> Bool
-reachesEnd dirs end = reachesEndRaw (0, 0) dirs end
-
-genList :: Point -> [[Direction]]
-genList size@(w, h) = filter (\x -> reachesEnd x size) (replicateM (fromIntegral (w + h) :: Int) [GoRight, GoDown])
+genPossibilities :: Integer -> Integer
+genPossibilities n = (fac (2 * n)) `div` ((fac n) ^ 2)
 
 -- Call the main function to get the result
-main = length $ genList (20, 20)
+main = genPossibilities 20
