1e0f1ed06b7e239644dc61e5946dcad2788a36aa
Crockeo <crockeo@gmail.com>
2013-07-03 00:32:35 -0400

    Finished project euler problem 18.

--- Euler018.hs
+++ Euler018.hs
@@ -9,7 +9,9 @@
 -- Change the N to the current problem
 module Euler18 where
 
+import Control.Applicative
 import Control.Monad
+
 import Data.Maybe
 
 type Point = (Int, Int)
@@ -22,7 +24,7 @@ data Direction = GoLeft | GoRight
 
 toPoint :: Direction -> Point
 toPoint GoLeft = (0, 1)
-toPoint GoRight = (1, 0)
+toPoint GoRight = (1, 1)
 
 moveDirection :: Direction -> Point -> Point
 moveDirection dir pos = translate (toPoint dir) pos
@@ -42,11 +44,15 @@ getElem triangle (x, y)
   | otherwise = Nothing
 
 traverseTriangleRaw :: Triangle -> Path -> Point -> Int
-traverseTriangleRaw triangle [] point = 0
-traverseTriangleRaw triangle (x:xs) point
-  | currentelem == Nothing = 0
-  | otherwise = (fromJust currentelem) + (traverseTriangleRaw triangle xs (moveDirection x point))
-  where currentelem = getElem triangle point
+traverseTriangleRaw triangle [] pos
+  | currelem == Nothing = 0
+  | otherwise = fromJust currelem
+  where currelem = getElem triangle pos
+
+traverseTriangleRaw triangle (x:xs) pos
+  | currelem == Nothing = 0
+  | otherwise = (fromJust currelem) + (traverseTriangleRaw triangle xs (moveDirection x pos))
+  where currelem = getElem triangle pos
 
 traverseTriangle :: Triangle -> Path -> Int
 traverseTriangle triangle path = traverseTriangleRaw triangle path (0, 0) 
@@ -57,9 +63,6 @@ traverseTriangleAll triangle paths = [traverseTriangle triangle x | x <- paths]
 genPaths :: Triangle -> [Path]
 genPaths triangle = replicateM ((triangleHeight triangle) - 1) [GoLeft, GoRight] 
 
-problemTriangle :: Triangle
-problemTriangle = [[75], [95, 64], [17, 47, 82], [18, 35, 87, 10], [20, 04, 82, 47, 65], [19, 01, 23, 75, 03, 34], [88, 02, 77, 73, 07, 63, 67], [99, 65, 04, 28, 06, 16, 70, 92], [41, 41, 26, 56, 83, 40, 80, 70, 33], [41, 48, 72, 33, 47, 32, 37, 16, 94, 29], [53, 71, 44, 65, 25, 43, 91, 52, 97, 51, 14], [70, 11, 33, 28, 77, 73, 17, 78, 39, 68, 17, 57], [91, 71, 52, 38, 17, 14, 91, 43, 58, 50, 27, 29, 48], [63, 66, 04, 68, 89, 53, 67, 30, 73, 16, 69, 87, 40, 31], [04, 62, 98, 27, 23, 09, 70, 98, 73, 93, 38, 53, 60, 04, 23]]
-
 greatest :: [Int] -> Int
 greatest [] = 0
 greatest (x:xs)
@@ -67,5 +70,11 @@ greatest (x:xs)
   | otherwise = next
   where next = greatest xs
 
+findGreatestPath :: Triangle -> Int
+findGreatestPath triangle = greatest $ traverseTriangleAll triangle $ genPaths triangle
+
+problemTriangle :: Triangle
+problemTriangle = [[75], [95, 64], [17, 47, 82], [18, 35, 87, 10], [20, 04, 82, 47, 65], [19, 01, 23, 75, 03, 34], [88, 02, 77, 73, 07, 63, 67], [99, 65, 04, 28, 06, 16, 70, 92], [41, 41, 26, 56, 83, 40, 80, 70, 33], [41, 48, 72, 33, 47, 32, 37, 16, 94, 29], [53, 71, 44, 65, 25, 43, 91, 52, 97, 51, 14], [70, 11, 33, 28, 77, 73, 17, 78, 39, 68, 17, 57], [91, 71, 52, 38, 17, 14, 91, 43, 58, 50, 27, 29, 48], [63, 66, 04, 68, 89, 53, 67, 30, 73, 16, 69, 87, 40, 31], [04, 62, 98, 27, 23, 09, 70, 98, 73, 93, 38, 53, 60, 04, 23]]
+
 -- Call the main function to get the result
-main = greatest $ traverseTriangleAll problemTriangle $ genPaths problemTriangle
+main = findGreatestPath problemTriangle
