d35392e24143fa89ee11032de87f993db2b28a67
Crockeo <crockeo@gmail.com>
2013-07-06 09:16:18 -0400

    Updated a solution to the 19th project euler problem.

--- Euler019.hs
+++ Euler019.hs
@@ -12,22 +12,78 @@ module Euler19 where
 import Data.Maybe
 import Data.List
 
-data Day = Saturday | Sunday | Monday | Tuesday | Wednesday | Thursday | Friday
+data Day = Monday | Tuesday | Wednesday | Thursday | Friday | Saturday | Sunday
   deriving (Eq, Ord, Enum, Show, Read)
-
-toDay :: Int -> Day
-toDay day = [Saturday .. Friday] !! day
-
-data Month = March | April | May | June | July | August | September | October | November | December | January | February
+type DayNum = Int
+data Month = January | February | March | April | May | June | July | August | September | October | November | December
   deriving (Eq, Ord, Enum, Show, Read)
-
-toNum :: Month -> Int
-toNum month = (fromJust (elemIndex month [March .. January])) + 3 
-
 type Year = Int
 
-calcDay :: Int -> Month -> Year -> Day
-calcDay = undefined
+type Date = (DayNum, Day, Month, Year)
+
+nextDay :: Day -> Day
+nextDay Monday    = Tuesday
+nextDay Tuesday   = Wednesday
+nextDay Wednesday = Thursday
+nextDay Thursday  = Friday
+nextDay Friday    = Saturday
+nextDay Saturday  = Sunday
+nextDay Sunday    = Monday
+
+nextMonth :: Month -> Month
+nextMonth January   = February
+nextMonth February  = March
+nextMonth March     = April
+nextMonth April     = May
+nextMonth May       = June
+nextMonth June      = July
+nextMonth July      = August
+nextMonth August    = September
+nextMonth September = October
+nextMonth October   = November
+nextMonth November  = December
+nextMonth December  = January
+
+monthLength :: Month -> Year -> Int
+monthLength January   year = 31
+monthLength February  year =
+  if isLeapYear year
+    then 29
+    else 28
+monthLength March     year = 31
+monthLength April     year = 30
+monthLength May       year = 31
+monthLength June      year = 30
+monthLength July      year = 31
+monthLength August    year = 31
+monthLength September year = 30
+monthLength October   year = 31
+monthLength November  year = 30
+monthLength December  year = 31
+
+isLeapYear :: Year -> Bool
+isLeapYear year = ((year `mod` 4 == 0) && (year `mod` 100 /= 0)) || (year `mod` 400 == 0)
+
+nextDate :: Date -> Date
+nextDate date@(daynum, day, month, year)
+  | (nextdaynum > monthLength month year) && (nextmonth == January) = (1, nextday, nextmonth, nextyear)
+  | (nextdaynum > monthLength month year) = (1, nextday, nextmonth, year)
+  | otherwise = (nextdaynum, nextday, month, year)
+  where nextdaynum = daynum + 1
+        nextday    = nextDay day
+        nextmonth  = nextMonth month
+        nextyear   = year + 1
+
+isTargetDay :: Date -> Bool
+isTargetDay (1, Sunday, month, year) = True
+isTargetDay date = False
+
+calcLoop :: Date -> Int
+calcLoop date@(daynum, day, month, 2000) = 0
+calcLoop date =
+  if isTargetDay date
+    then 1 + (calcLoop (nextDate date))
+    else (calcLoop (nextDate date))
 
 -- Call the main function to get the result
-main = undefined
+main = calcLoop (1, Tuesday, January, 1901)
