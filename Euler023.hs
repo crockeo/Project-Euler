596005e74909637c780deefdb11a9ff995f9ddb1
Crockeo <crockeo@gmail.com>
2013-07-14 01:44:29 -0400

    Improved, but not solved, Project Euler problem 23 attempted solution.

--- Euler023.hs
+++ Euler023.hs
@@ -11,16 +11,22 @@ module Euler23 where
 
 import Data.List
 
--- Stripping the copies from a given list
-stripCopies :: (Eq a) => [a] -> [a] -> [a]
-stripCopies [] prev = []
-stripCopies (x:xs) prev
-  | elem x prev = stripCopies xs prev
-  | otherwise   = x : stripCopies xs (x : prev)
+elemQuick :: (Eq a, Ord a) => a -> [a] -> Bool
+elemQuick n [] = False
+elemQuick n (x:xs)
+  | n < x     = False
+  | n == x    = True
+  | otherwise = elemQuick n xs
+
+anyCopies :: (Eq a) => [a] -> [a] -> Bool
+anyCopies [] prev = False
+anyCopies (x:xs) prev
+  | elem x prev = True
+  | otherwise   = anyCopies xs (x : prev)
 
 -- The cap for non-abundant-sum numbers
 cap :: Int
-cap = 28123
+cap = 20161
 
 -- Finding the divisors of a given number
 divisorsRaw :: Int -> Int -> [Int]
@@ -30,7 +36,7 @@ divisorsRaw n d
   | otherwise                        = []
 
 divisors :: Int -> [Int]
-divisors n = sort $ stripCopies (divisorsRaw n 1) []
+divisors n = sort $ nub $ divisorsRaw n 1
 
 -- Finding the proper divisors (the divisors minus the number itself)
 propDivisors :: Int -> [Int]
@@ -40,18 +46,27 @@ propDivisors n = init $ divisors n
 isAbundantNumber :: Int -> Bool
 isAbundantNumber n = (sum $ propDivisors n) > n
 
+-- Checking if a tuple of two integers are both abundant
+areBothAbundant :: (Int, Int) -> Bool
+areBothAbundant (n1, n2) = isAbundantNumber n1 && isAbundantNumber n2
+
+-- Get addses
+getAddses :: Int -> [(Int, Int)]
+getAddses n = zip [1 .. n `div` 2] [n - 1, n - 2 .. n `div` 2] 
+
 -- A list of abundant numbers
 abundantNumbers :: [Int]
 abundantNumbers = [n | n <- [1 .. cap], isAbundantNumber n]
 
 -- The sum of abundant numbers
--- TODO: Efficiently generate the sums of abundant numbers
 abundantSums :: [Int]
-abundantSums = undefined
+abundantSums = [(abundantNumbers !! x) + (abundantNumbers !! y) | x <- [0 .. (length abundantNumbers) - 1], y <- [x .. (length abundantNumbers) - 1]]
 
 -- Checking if a number is an abundant sum
 isAbundantSum :: Int -> Bool
-isAbundantSum n = elem n abundantSums
+isAbundantSum n
+  | (n > 48) && (n `mod` 2 == 0) = True
+  | otherwise                    = (filter (areBothAbundant) $ getAddses n) /= []
 
 -- Call the main function to get the result
 main = sum [n | n <- [1 .. cap], not $ isAbundantSum n]
