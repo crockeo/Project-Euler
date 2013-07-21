4ee648ca9fd3e5c3b19014091fab867668c7fce0
Crockeo <crockeo@gmail.com>
2013-07-21 00:37:16 -0400

    Changed problem 2 to be solved with the utils library.

--- Euler002.hs
+++ Euler002.hs
@@ -10,17 +10,7 @@
 -- Change the N to the current problem
 module Euler2 where
 
-generateFibonacciToLoop a b c array to = do
-	if c >= to
-		then array
-	else (generateFibonacciToLoop b c (b + c) (c : array) to)
-
-generateFibonacciTo to = generateFibonacciToLoop 1 1 2 [1, 1] to
-
-e2 = sum
-	(filter
-		(\x -> x `mod` 2 == 0)
-		(generateFibonacciTo 4000000))
+import Utils
 
 -- Call the main function to get the result
-main = print (e2)
+main = sum $ filter (\x -> x `mod` 2 == 0) $ takeWhile (\x -> x < 4000000) fibonaccis
