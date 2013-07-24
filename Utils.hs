154e87a4c9938546f501e8c3563dce34d80cf78b
Crockeo <crockeo@gmail.com>
2013-07-24 12:41:57 -0400

    Made Utils more general (taking typeclasses rather than concrete types.

--- Utils.hs
+++ Utils.hs
@@ -8,21 +8,16 @@
 
 module Utils where
 
--- Converting an int to a float
-toFloat :: Int -> Float
-toFloat n = (fromInteger $ toInteger n) :: Float
-
-toInt :: Float -> Int
-toInt = floor
+import Data.List
 
 -- Checking if a number is prime
-isPrimeRaw :: Int -> Int -> Bool
+isPrimeRaw :: (Integral a) => a -> a -> Bool
 isPrimeRaw n x
   | x * x > n = True
   | n `mod` x == 0 = False
   | otherwise = isPrimeRaw n (x + 1)
 
-isPrime :: Int -> Bool
+isPrime :: (Integral a) => a -> Bool
 isPrime 0 = False
 isPrime 1 = False
 isPrime n = isPrimeRaw n 2
@@ -33,22 +28,33 @@ splitNumRaw 0 = []
 splitNumRaw n = (n `mod` 10) : splitNumRaw (n `div` 10)
 
 splitNum :: (Integral a) => a -> [a]
-splitNum n = reverse $ splitNumRaw n
+splitNum n
+  | n < 0     = []
+  | otherwise = reverse $ splitNumRaw n
 
 joinNum :: (Integral a) => [a] -> a
 joinNum [] = 0
 joinNum (x:xs) = (10 ^ (length xs)) * x + joinNum xs
 
+joinBigNums :: (Integral a) => [a] -> a
+joinBigNums list =
+  joinNum $ concat slist
+  where slist = map (splitNum) list
+
+-- Getting the length of a number
+numLength :: (Integral a) => a -> Int
+numLength = length . splitNum
+
 -- A list of factorials
-factorials :: [Integer]
-factorials = 1 : [n * factorials !! ((fromInteger n :: Int) - 1) | n <- [1 ..]]
+factorials :: (Integral a) => [a]
+factorials = scanl (*) 1 [1 ..]
 
 -- A list of fibonacci terms
-fibonaccis :: [Integer]
-fibonaccis = 0 : 1 : [fibonaccis !! ((fromInteger n :: Int) - 1) + fibonaccis !! ((fromInteger n :: Int) - 2) | n <- [2 ..]]
+fibonaccis :: (Integral a) => [a]
+fibonaccis = scanl (+) 0 (1 : fibonaccis)
 
 -- A list of primes
-primes :: [Int]
+primes :: (Integral a) => [a]
 primes = [n | n <- [2 ..], isPrime n]
 
 -- Search functions
@@ -64,11 +70,11 @@ allElem :: (Eq a) => [a] -> [a] -> Bool
 allElem check to = and $ map (\x -> elem x to) check
 
 -- Checking if a number is pandigital
-isPandigital :: Int -> Bool
+isPandigital :: (Integral a) => a -> Bool
 isPandigital n =
-  allElem [1 .. (length sn)] sn
+  allElem [1 .. fromIntegral $ length sn] sn
   where sn = splitNum n
 
 -- Getting the approximate sqrt of a number floor(sqrt(n))
-cleanSqrt :: Int -> Int
-cleanSqrt n = toInt $ sqrt $ toFloat n
+cleanSqrt :: (Integral a) => a -> a
+cleanSqrt n = truncate $ (sqrt . fromIntegral) n
