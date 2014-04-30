474e3085b3e708641f4e1111c7bccdbb6b887304
Cerek Hillen <crockeo@gmail.com>
2014-04-30 11:46:33 -0400

    Made Euler 48 rely upon Util functions.

--- Euler048.hs
+++ Euler048.hs
@@ -9,25 +9,13 @@
 -- Change the N to the current problem
 module Euler48 where
 
+import Utils
+
 -- Calculating the sums of a self-power
-calcN :: Integer -> Integer
+calcN :: (Integral a) => a -> a
 calcN n =
   sum [x ^ x | x <- [1 .. n]]
-
--- Splitting a number int a list (returns the number reversed)
-splitList :: Integer -> [Integer]
-splitList 0 = []
-splitList n =
-  (n `mod` 10) : (splitList (n `div` 10))
-
--- Conjoining a (reversed) list into a number
-combineList :: [Integer] -> Integer
-combineList l =
-  combineListRaw l 1
-  where combineListRaw  [] _    = 0
-        combineListRaw (x:xs) n =
-          (x * n) + (combineListRaw xs (n * 10))
           
 -- Call the main function to get the result
 main =
-  combineList $ take 10 $ splitList $ calcN 1000
+  joinNumBackwards $ take 10 $ splitNumBackwards $ calcN 1000
